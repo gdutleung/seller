c234aa7d347de54bfa19b7d7ccee910d082e98c2
lpr <915518319@qq.com>
2017-11-03 11:23:01 +0800

    the first commit about a seller app

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <!--<div class="header">-->
+      <v-header :seller="seller"></v-header>
+    <!--</div>-->
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <div class="content">
+      <router-view :seller="seller"></router-view>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from 'components/header/header.vue'
+
+  export default {
+    data () {
+      return {
+        seller: {}
+      }
+    },
+    created () {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body
+        if (response.errno === 0) {
+          this.seller = response.data
+        }
+      })
+    },
+    components: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" type="text/stylus">
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display flex
+    width 100%
+    height 40px
+    line-height 40px
+    /*border-bottom 1px solid #999*/
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex 1
+      text-align center
+      & > a
+        display block
+        font-size 14px
+        color rgb(77, 85, 93)
+        &.active
+          color rgb(240, 20, 20)
+</style>
